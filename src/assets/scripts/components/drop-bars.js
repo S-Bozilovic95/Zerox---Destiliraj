804bc90d94a97b6ce477825ff07470c7175e7b96
Lacedaimon95 <svetozar95@gmail.com>
2023-01-11 14:46:44 +0100

    fix: added style fixes

--- src/assets/scripts/components/drop-bars.js
+++ src/assets/scripts/components/drop-bars.js
@@ -1,18 +1,16 @@
 /* eslint-disable no-unused-vars */
-let icon = document.querySelectorAll('.drop-bars__bars-box__bar__tab__icon');
-let title = document.querySelectorAll('.drop-bars__bars-box__bar__tab__desc');
+let tab = document.querySelectorAll('.drop-bars__bars-box__bar__tab');
 
 // /functions
 function toggleBar (value) {
     value.forEach(el => {
         el.addEventListener('click', (e) => {
-            e.target.parentNode.parentNode.classList.toggle('bar-active');
+            e.target.parentNode.classList.toggle('bar-active');
         });
     });
 }
 
 // toggle bars
-if (icon && title) {
-    toggleBar(icon);
-    toggleBar(title);
+if (tab) {
+    toggleBar(tab);
 }
